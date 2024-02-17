e11e2f476df95ede2bf207a032791c9c94b32c98
hassanxelamin <hello@hassanelamin.co>
2024-02-16 19:33:42 -0800

    x

--- client/src/components/slider.jsx
+++ client/src/components/slider.jsx
@@ -2,18 +2,30 @@ import React, { useState, useEffect } from 'react'
 import SliderItem from './slider-item';
 
 const Slider = () => {
-
   const [images, setImages] = useState([]);
-  
+  const [currentIndex, setCurrentIndex] = useState();
   const [selectedMovie, setSelectedMovie] = useState("Batman Begins");
-  const [translateX, setTranslateX] = useState(-11.5);
+
+  const gapSizeRem = 0.5;
+  const rootFontSize = 10;
+  const gapSizePixels = gapSizeRem * rootFontSize;
+  const viewportWidth = window.innerWidth;
+  const gapSizePercentage = (gapSizePixels / viewportWidth) * 100;
+  const totalMovePerItem = 15 + gapSizePercentage;
   
   useEffect(() => {
     const fetchRecommendations = async () => {
       try {
         const response = await fetch(`http://localhost:8000/recommendations/${selectedMovie}`);
         const data = await response.json();
-        setImages(data.posters); 
+
+        // const clonedStart = data.posters.slice(0, 6); 
+        // const clonedEnd = data.posters.slice(-6);
+        // const loopedImages = [...clonedEnd, ...data.posters, ...clonedStart];
+
+        setImages(data.posters);
+        setCurrentIndex(-0.3); // Start at the first real image, after the cloned end images
+
       } catch (error) {
         console.error("Error fetching recommendations:", error);
       }
@@ -22,13 +34,16 @@ const Slider = () => {
   }, [selectedMovie]);
 
   const handleLeftClick = () => {
-    setTranslateX(translateX + 91.5);
-  }
-  
+    setCurrentIndex(currentIndex - 6);
+  };
 
   const handleRightClick = () => {
-    setTranslateX(translateX - 91.5);
-  }
+    setCurrentIndex(currentIndex + 6);
+  };
+
+  // Calculate the transform value based on the current index
+  const transformValue = -(totalMovePerItem * currentIndex);
+
 
   return (
     <section className='w-full flex justify-center items-center'>
@@ -42,7 +57,7 @@ const Slider = () => {
             Arrow
           </button>
 
-          <div id="slider" data-name="slider" style={{ transform: `translateX(${translateX}%)` }} className={`flex gap-x-2 transition ease-in-out delay-150`}>
+          <div id="slider" data-name="slider" style={{ transform: `translateX(${transformValue}%)` }} className={`flex gap-x-2 transition ease-in-out delay-150`}>
             {images.map((image, index) => (
               <SliderItem key={index} index={index} image={image} />
               ))}
